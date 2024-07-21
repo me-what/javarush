9f7645796532eb5f5973db4d16754a33bfd8bf98
me-what <eldar.979751@gmail.com>
2024-07-21 12:01:33 +0300

    10

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
 
+        boolean isYoung = age <= 20 || age >= 60;
+        boolean isOld = (age > 20 || age <= 60);
+
+        if (isYoung || !isOld) {
+            System.out.println("можно не работать");
+        }
     }
 }
