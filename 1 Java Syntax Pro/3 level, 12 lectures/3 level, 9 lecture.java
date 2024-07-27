1fb9188f4b0dc86a79823cf0349b8aa108b0f76c
me-what <eldar.979751@gmail.com>
2024-07-27 15:08:57 +0300

    10

--- 1 Java Syntax Pro/3 level, 12 lectures/3 level, 9 lecture.java
+++ 1 Java Syntax Pro/3 level, 12 lectures/3 level, 9 lecture.java
@@ -30,7 +30,7 @@ public class Main {
 }
 
 
-// Task 1. Сравним строки
+// Task 1. Сравним строки. Compare the strings.
 /* Напиши программу, которая считывает с клавиатуры две строки и выдает сообщение о том, одинаковые ли эти строки. */
 import java.util.Scanner;
 
@@ -49,7 +49,7 @@ public class Main {
 }
 
 
-// Task 2. Сравнение строк по ссылке
+// Task 2. Сравнение строк по ссылке. Comparing strings by reference.
 /* В методе main напиши код попарного сравнения по ссылке строк string1, string2 и string3 с выводом соответствующего
 сообщения после каждого сравнения:
 "ссылки на строки одинаковые" или "ссылки на строки разные".
@@ -83,5 +83,55 @@ public class Main {
         } else {
             System.out.println(different);
         }
+    }
+}
+
+
+// Task 3. Сломанная клавиатура. A broken keyboard.
+/* Амиго создал секретное слово для доступа к закрытой информации, но у него сломалась клавиатура, и теперь невозможно
+набирать буквы в верхнем регистре. Напиши программу, которая будет сравнивать введенную строку со строкой из переменной
+secret, не учитывая регистр.
+Если введенная строка равна строке из переменной secret, программа выводит на экран сообщение "доступ разрешен".
+В ином случае - "доступ запрещен". */
+import java.util.Scanner;
+
+public class Main {
+    public static String secret = "AmIGo";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        if (string.equalsIgnoreCase(secret)) {
+            System.out.println("доступ разрешен");
+        } else {
+            System.out.println("доступ запрещен");
+        }
+    }
+}
+
+
+// exapmple
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String a = console.nextLine();
+        String b = console.nextLine();
+        String result = a.equals(b) ? "Одинаковые" : "Разные";
+        System.out.println(result);
+    }
+}
+
+/* Интересный нюанс сравнения строк
+Есть один важный нюанс, о котором вам нужно знать.
+
+Если Java-компилятор в вашем коде (именно в коде) найдет несколько одинаковых строк, для экономии памяти он
+создаст для них только один объект. */
+public class Main {
+    public static void main(String[] args) {
+        String text = "Это очень важное сообщение";
+        String message = "Это очень важное сообщение";
+        System.out.println(text==message); // true
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
-import java.util.Scanner;
 
 public class Main {
-    public static String string1 = "Амиго";
-    public static String string2 = string1;
-    public static String string3 = new String(string1);
-
     public static void main(String[] args) {
-        String same = "ссылки на строки одинаковые";
-        String different = "ссылки на строки разные";
-        if (string1==string2) { // true
-            System.out.println(same);
-        } else {
-            System.out.println(different);
-        }
-
-        if (string2==string3) { // false
-            System.out.println(same);
-        } else {
-            System.out.println(different);
-        }
-
-        if (string1==string3) { // false
-            System.out.println(same);
-        } else {
-            System.out.println(different);
-        }
+        String text = "Это очень важное сообщение";
+        String message = "Это очень важное сообщение";
+        System.out.println(text==message); // true
     }
 }
