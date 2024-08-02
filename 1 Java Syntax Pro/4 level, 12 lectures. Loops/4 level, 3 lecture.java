67f4ba1982b31eb1ff2db606231e6c9e37892405
me-what <eldar.979751@gmail.com>
2024-08-02 18:04:22 +0300

    10

--- 1 Java Syntax Pro/4 level, 12 lectures. Loops/4 level, 3 lecture.java
+++ 1 Java Syntax Pro/4 level, 12 lectures. Loops/4 level, 3 lecture.java
@@ -161,3 +161,144 @@ public class Main {
         System.out.println(sum);
     }
 }
+
+
+/* Задача 1. Допустим, мы хотим написать программу, которая бы выводила на экран 4 раза слово Мама.
+Цикл — это именно то, что нам нужно. И примерно так выглядел бы наш код: */
+public class Main {
+    public static void main(String[] args) {
+        int  n = 0;
+        while (n < 4)
+        {
+            System.out.println("Мама");
+            n++;
+        }
+    }
+}// Мама Мама Мама Мама
+
+
+/* Задача 2. Мы хотим написать программу, которая бы выводила в одну строку 5 букв А.
+Для этого нам опять нужен цикл. Вот как будет выглядеть этот код */
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 5)
+        {
+            System.out.print("A");
+            n++;
+        } // AAAAA
+    }
+}
+
+
+/* Задача 3. Мы хотим вывести на экран прямоугольник из букв А размером в 4 строки и 5 столбцов.
+А вот для этого нам уже нужны вложенные циклы. Просто возьмем первый пример, где мы выводим 4 строки и заменим код
+по выводу одной строки на код из второго примера. */
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+
+        while (n < 4)
+        {
+            int m = 0;
+            while (m < 5)
+            {
+                System.out.print("А");
+                m++;
+            }
+            System.out.println();
+            n++;
+        }
+    }
+}
+
+
+/* Task 4.
+Используя вложенные циклы while (цикл в цикле) выведи на экран прямоугольник размером 5 (высота) на 10 (ширина),
+заполненный буквой 'Q'.
+
+Пример вывода:
+QQQQQQQQQQ
+QQQQQQQQQQ
+QQQQQQQQQQ
+QQQQQQQQQQ
+QQQQQQQQQQ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a<5) {
+            int b = 0;
+
+            while (b<10) {
+                System.out.print("Q");
+                b++;
+            }
+            System.out.println();
+            a++;
+        }
+    }
+}
+
+
+/* Task 5. Незаполненный прямоугольник
+Используя вложенные циклы while (цикл в цикле) выведи на экран незаполненный прямоугольник (его контур)
+размером 10 (высота) на 20 (ширина) из букв 'Б'.
+Незаполненная часть состоит из пробелов.
+
+Пример вывода:
+ББББББББББББББББББББ
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+ББББББББББББББББББББ */
+// решение 1
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 10) {
+            int b1 = 0;
+            if (a == 1 || a == 2 || a == 3 || a == 4 || a == 5 || a == 6 || a == 7 || a == 8) {
+                while (b1 < 1) {
+                    System.out.print("Б                  Б");
+                    b1++;
+                }
+            } else {
+                while (b1 < 20) {
+                    System.out.print("Б");
+                    b1++;
+                }
+            }
+            System.out.println();
+            a++;
+        }
+    }
+}
+
+// решение 2
+public class Main {
+    public static void main(String[] args) {
+        int height = 1;
+        while (height <= 10) {
+            int width = 1;
+            while (width <= 20) {
+                if (height == 1 || height == 10) {
+                    System.out.print('Б');
+                } else if (width == 1 || width == 20) {
+                    System.out.print('Б');
+                } else {
+                    System.out.print(' ');
+                }
+                width++;
+            }
+            System.out.println();
+            height++;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,60 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-
-        int sum = 0;
-        boolean isExit = false;
-        while (!isExit) {
-            if (scanner.hasNextInt()) {
-                int number = scanner.nextInt();
-                sum = sum + number;
-            } else if (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.equals("ENTER")) {
-                    isExit = true;
+    public static void main(String[] args) {
+        int height = 1;
+        while (height <= 10) {
+            int width = 1;
+            while (width <= 20) {
+                if (height == 1 || height == 10) {
+                    System.out.print('Б');
+                } else if (width == 1 || width == 20) {
+                    System.out.print('Б');
+                } else {
+                    System.out.print(' ');
                 }
+                width++;
             }
+            System.out.println();
+            height++;
         }
-        System.out.println(sum);
     }
 }
+
+
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int a = 0;
+//
+//        while (a < 10) {
+//            int b1 = 0;
+//            if (a == 1 || a == 2 || a == 3 || a == 4 || a == 5 || a == 6 || a == 7 || a == 8) {
+//                while (b1 < 1) {
+//                    System.out.print("Б                  Б");
+//                    b1++;
+//                }
+//            } else {
+//                while (b1 < 20) {
+//                    System.out.print("Б");
+//                    b1++;
+//                }
+//            }
+//            System.out.println();
+//            a++;
+//        }
+//    }
+//}
+
+
+
+/* Пример вывода:
+ББББББББББББББББББББ
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+Б                  Б
+ББББББББББББББББББББ */
