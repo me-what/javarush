975a5a648fccf273cf2d3620fe786d0312b54667
me-what <eldar.979751@gmail.com>
2024-09-25 23:11:06 +0300

    10

--- 1 Java Syntax Pro/2 level, 10 lectures. Data type. Scanner/2 level, 3 lecture.java
+++ 1 Java Syntax Pro/2 level, 10 lectures. Data type. Scanner/2 level, 3 lecture.java
@@ -1,5 +1,13 @@
+// https://javarush.com/quests/lectures/questsyntaxpro.level02.lecture00
+// Тип int – целые числа
+
+// Task 1
+// «Я по объявлению…». "I'm on an ad..."
 public class Solution {
     public static void main(String[] args) {
         int intValue = 10, numericValue = 20, size = 30, number = 40;
     }
 }
+/* В методе main объяви переменные intValue, numericValue, size, number типа int.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые целые числа. */
